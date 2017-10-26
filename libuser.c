71a6b978285a1bf2deb49ef32af54f13f0cbcbf1
Caleb Short <Caleb@Calebs-MBP-2.PK5001Z>
2017-10-25 22:13:56 -0700

    Finished system call functions. But there are still some questions

--- libuser.c
+++ libuser.c
@@ -55,7 +55,22 @@ int DiskWrite(void *dbuff, int unit, int track, int first, int sectors, int *sta
     {
         USLOSS_Console("DiskWrite(): called.\n");
     }
-    return -1; // TODO
+    USLOSS_Sysargs sysArg;
+    CHECKMODE;
+    sysArg.number = SYS_DISKWRITE;
+    sysArg.arg1 = dbuff;
+    sysArg.arg2 = (void *) ((long) sectors);
+    sysArg.arg3 = (void *) ((long) track);
+    sysArg.arg4 = (void *) ((long) first);
+    sysArg.arg5 = (void *) ((long) unit);
+
+    USLOSS_Syscall(&sysArg);
+
+    // Return arg4 and put arg1 in status
+    *status = (int) ((long) sysArg.arg1);
+    int returnStatus = (int) ((long) sysArg.arg4);
+
+    return returnStatus;
 }
 
 int DiskSize(int unit, int *sector, int *track, int *disk)
@@ -64,7 +79,19 @@ int DiskSize(int unit, int *sector, int *track, int *disk)
     {
         USLOSS_Console("DiskSize(): called.\n");
     }
-    return -1; // TODO
+    USLOSS_Sysargs sysArg;
+    CHECKMODE;
+    sysArg.number = SYS_DISKSIZE;
+    sysArg.arg1 = (void *) ((long) unit);
+
+    USLOSS_Syscall(&sysArg);
+
+    *sector = (int) ((long) sysArg.arg2);
+    *track = (int) ((long) sysArg.arg3);
+    *disk = (int) ((long) sysArg.arg1);   // TODO: Is this right? This should be the size of a sector
+    int returnStatus = (int) ((long) sysArg.arg4);
+
+    return returnStatus;
 }
 
 int TermRead(char *buff, int bsize, int unit_id, int *nread)
@@ -73,14 +100,39 @@ int TermRead(char *buff, int bsize, int unit_id, int *nread)
     {
         USLOSS_Console("TermRead(): called.\n");
     }
-    return -1; // TODO
+    USLOSS_Sysargs sysArg;
+    CHECKMODE;
+    sysArg.number = SYS_TERMREAD;
+    sysArg.arg1 = (void *) buff;
+    sysArg.arg2 = (void *) ((long) bsize);
+    sysArg.arg3 = (void *) ((long) unit_id);
+
+    USLOSS_Syscall(&sysArg);
+
+    *nread = (int) ((long) sysArg.arg2);
+    int returnStatus = (int) ((long) sysArg.arg4);
+
+    return returnStatus;
 }
 
 int TermWrite(char *buff, int bsize, int unit_id, int *nwrite)
 {
+    // TODO: What is bsize for?
     if(DEBUG4 && debugflag4)
     {
         USLOSS_Console("TermWrite(): called.\n");
     }
-    return -1; // TODO
+    USLOSS_Sysargs sysArg;
+    CHECKMODE;
+    sysArg.number = SYS_TERMWRITE;
+    sysArg.arg1 = (void *) buff;
+    sysArg.arg2 = (void *) ((long) (nwrite));
+    sysArg.arg3 = (void *) ((long) unit_id);
+
+    USLOSS_Syscall(&sysArg);
+
+    *nwrite = (int) ((long) sysArg.arg2);
+    int returnStatus = (int) ((long) sysArg.arg4);
+
+    return returnStatus; 
 }
